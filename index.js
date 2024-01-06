b97e2968bd2a8980040d567498cdee0c085fc583
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2024-01-06 20:31:48 +0600

    testimonial show

--- index.js
+++ index.js
@@ -209,6 +209,12 @@ async function run() {
       res.send(result)
     });
 
+    
+    app.get("/testimonials", async (req, res) => {
+      const result = await testimonialCollection.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
